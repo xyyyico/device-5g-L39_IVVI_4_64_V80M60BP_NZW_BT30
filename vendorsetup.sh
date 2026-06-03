2b603365c72d2ece42a8151ddd4a88f187e931e6
xyyyico <65223365+xyyyico@users.noreply.github.com>
2026-06-03 23:20:41 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# 旧语法已废弃，已改用 AndroidProducts.mk
+add_lunch_combo twrp_L39_IVVI_4_64_V80M60BP_NZW_BT30-eng
