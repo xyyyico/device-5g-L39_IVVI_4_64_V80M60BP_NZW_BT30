2ded3e2e363505d4c23a46ca8743465bd1ec6c18
xyyyico <65223365+xyyyico@users.noreply.github.com>
2026-06-04 08:50:56 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_DIR := $(call my-dir)
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_L39_IVVI_4_64_V80M60BP_NZW_BT30.mk
-
+$(LOCAL_DIR)/twrp_L39_IVVI_4_64_V80M60BP_NZW_BT30.mk
 COMMON_LUNCH_CHOICES := \
-    twrp_L39_IVVI_4_64_V80M60BP_NZW_BT30-eng
+twrp_L39_IVVI_4_64_V80M60BP_NZW_BT30-eng
