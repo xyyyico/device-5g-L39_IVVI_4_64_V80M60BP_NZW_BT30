7f31413411484d6bc5fbb0c637348880a19259db
xyyyico <65223365+xyyyico@users.noreply.github.com>
2026-06-16 23:49:20 +0800

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# 新版AOSP废弃vendorsetup注册机型，改用AndroidProducts.mk
-# 此处仅保留环境变量，不再添加lunch combo
