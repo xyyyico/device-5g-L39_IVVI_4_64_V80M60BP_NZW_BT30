c4a0cd6a6525442fff4aed306a91331ce3c806d5
xyyyico <65223365+xyyyico@users.noreply.github.com>
2026-06-20 13:20:18 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,4 +1,3 @@
-
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
@@ -8,6 +7,9 @@
 
 # 定义当前设备树所在路径（标准变量 DEVICE_PATH）
 DEVICE_PATH := device/5g/L39_IVVI_4_64_V80M60BP_NZW_BT30
+
+# ====================== A/B 分区全局总开关（必须前置，缺失会导致OTA失效） ======================
+AB_OTA_UPDATER := true
 
 # --------------------------
 # A/B 分区 OTA 升级配置
@@ -39,15 +41,17 @@ PRODUCT_PACKAGES += \
 	update_engine_sideload
 
 # ====================== 修复文件拷贝项 ======================
-# 1. 将原厂fstab.mt6768复制到recovery ramdisk，解决 Unable to parse vendor fstab
+# 1. fstab仅拷贝至first_stage_ramdisk，删除recovery根目录冗余拷贝，避免分区重复挂载冲突
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/fstab.mt6768:recovery/root/fstab.mt6768 \
     $(DEVICE_PATH)/fstab.mt6768:recovery/root/first_stage_ramdisk/fstab.mt6768
 
 # 2. 拷贝自定义recovery.fstab到recovery根目录（TWRP/OF标准读取路径）
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/recovery.fstab:recovery/root/recovery.fstab
 
-# 3. 【SELinux权限修复】仅拷贝编译好的cil规则到ramdisk
+# 3. 【SELinux修复】cil直接放到recovery根目录，不再创建sepolicy文件夹，解决 rm: Is a directory 编译报错
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/sepolicy/recovery_sepolicy.cil:recovery/root/sepolicy/recovery_sepolicy.cil
+    $(DEVICE_PATH)/sepolicy/recovery_sepolicy.cil:recovery/root/recovery_sepolicy.cil
+
+# 声明recovery加载自定义cil规则（关键，否则SELinux规则不生效）
+BOARD_RECOVERY_SEPOLICY += $(TARGET_RECOVERY_ROOT_OUT)/recovery_sepolicy.cil
